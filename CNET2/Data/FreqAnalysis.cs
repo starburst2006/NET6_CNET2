c62fcf26821f6b19e96163621e306a8dacbbcd3b
Marek Bohanes <m.bohanes@gmail.com>
2022-06-13 13:40:16 +0200

    library Data, přidány funkce FreqAnalysisForm...

---
+++ CNET2/Data/FreqAnalysis.cs
@@ -0,0 +1,25 @@
+﻿namespace Data
+{
+    public class FreqAnalysis
+    {
+        public static Dictionary<string, int> FreqAnalysisFromString(string imput)
+        {
+            throw new NotImplementedException();  // nemám dopsáno, kompilátor mě nepustí zkompilovat, proto se dá obejít tímto zápisem...
+        }
+
+        public static async Task<Dictionary<string, int>> FreqAnalysisFromUrl(string url)
+        {
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync(url);
+            return FreqAnalysisFromString(content);
+        }
+
+        public static Dictionary<string, int> FreqAnalysisFromFile(string file)
+        {
+            var content = File.ReadAllText(file);
+            return FreqAnalysisFromString(content);
+        }
+
+
+    }
+}
