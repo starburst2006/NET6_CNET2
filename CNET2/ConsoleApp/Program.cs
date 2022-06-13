eaa31259dae71931205c66a11337e5c86b1eac2f
Marek Bohanes <m.bohanes@gmail.com>
2022-06-13 16:26:22 +0200

    úkol 4 pomocí LINQ

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 
 
-int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
+//int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
 
 // where filtrace
 //var result = numbers.Where(x => x >= 1 && x <= 99);
@@ -27,39 +27,63 @@ int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
 //var result = numbers.Select(n => Math.Abs(n));
 
 
+//foreach (int item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+
+
+//úkol 1: zjistěte počet kladných čísel
+//var result = numbers.Where(x => x>=0).Count();
+//Console.WriteLine(result);
+
+//úkol 2: ignorujte největší a nejmenší číslo a ze zbytku vypočítej průměr
+//var min = numbers.Min();
+//var max = numbers.Max();
+//var zbytek = numbers.Where(x => x != min && x != max);
+//var avg = zbytek.Average();
+//Console.WriteLine(avg);
+
+//úkol 3: spočítat liché a sudé
+//var liche = numbers.Where(x => x % 2 != 0).Count();
+//var sude = numbers.Where(x => x % 2 == 0).Count();
+//Console.WriteLine($"sudé: {sude}, liché: {liche}");
+
+
+//úkol 4: vypište pole numbers pomocí slovního vyhádření z pole stringů 
+var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+var strings = new[] { "zero", "one", "two", "three", "four",
+    "five", "six", "seven", "eight", "nine" };
+
+var result = numbers.Select(x => strings[x]);
+
+    Console.WriteLine(string.Join(", ", result ));
 
 
 
 
 
+//static void FreqWords()
+//{
+//    var booksdir = @"C:\Users\Student\source\repos\starburst2006\Books";
 
+//    var files = Directory.EnumerateFiles(booksdir, "*.txt");
+//    foreach (var file in files)
+//    {
+//        var result = FreqAnalysis.FreqAnalysisFromFile(file);
+//        var fileInfo = new FileInfo(file);
 
+//        Console.WriteLine(fileInfo.Name);
 
-foreach (int item in result)
-{
-    Console.WriteLine(item);
-}
+//        var orderedTop10 = result.Words.OrderByDescending(kv => kv.Value).Take(10);
 
+//        foreach (var item in orderedTop10)
+//        {
+//            Console.WriteLine($"{item.Key}: {item.Value}");
+//        }
 
-static void FreqWords()
-{
-    var booksdir = @"C:\Users\Student\source\repos\starburst2006\Books";
-
-    var files = Directory.EnumerateFiles(booksdir, "*.txt");
-    foreach (var file in files)
-    {
-        var result = FreqAnalysis.FreqAnalysisFromFile(file);
-        var fileInfo = new FileInfo(file);
-
-        Console.WriteLine(fileInfo.Name);
-
-        var orderedTop10 = result.Words.OrderByDescending(kv => kv.Value).Take(10);
-
-        foreach (var item in orderedTop10)
-        {
-            Console.WriteLine($"{item.Key}: {item.Value}");
-        }
-
-        Console.WriteLine();
-    }
-}
+//        Console.WriteLine();
+//    }
+//}
