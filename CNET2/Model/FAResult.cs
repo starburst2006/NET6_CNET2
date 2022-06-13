575d3e7e09fda028afd7fd295b4fdb3d0e5a09e7
Marek Bohanes <m.bohanes@gmail.com>
2022-06-13 11:51:57 +0200

    deklarována třída FAReslt

---
+++ CNET2/Model/FAResult.cs
@@ -0,0 +1,31 @@
+﻿namespace Model
+{
+    /// <summary>
+    /// Výsledek frekvenční analýzy pro jeden zdroj (soubor nebo URL)
+    /// </summary>
+    public class FAResult
+    {
+        
+        /// <summary>
+        /// zdroj textu
+        /// </summary>
+        public string Source { get; set; }
+        public SourceType SourceType { get; set; }
+        
+        /// <summary>
+        /// výsledná frekvenční analýza slov
+        /// </summary>
+        public Dictionary<string, int> Words { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Source} {Words?.Count}";
+        }
+    }
+
+    public enum SourceType
+    {
+        URL,
+        FILE
+    }
+}
