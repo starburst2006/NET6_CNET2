7a4aaf76447be87bb048fbf7583637fd24b91426
Marek Bohanes <m.bohanes@gmail.com>
2022-06-13 15:57:35 +0200

    Linq základní metody

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -1,24 +1,65 @@
 ﻿using Data;
 using Model;
+using System.Linq;
 
 
 
-var booksdir = @"C:\Users\Student\source\repos\starburst2006\Books";
 
-var files = Directory.EnumerateFiles(booksdir, "*.txt");
-foreach(var file in files)
+int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
+
+// where filtrace
+//var result = numbers.Where(x => x >= 1 && x <= 99);
+
+//orderby řadí
+//var result = numbers.OrderBy(n => n);
+
+//Agregace min, max, sum, avg, count
+//var result = numbers.Max();
+//var result = numbers.Min();
+//var result = numbers.Sum();
+//var result = numbers.Average();
+//var result = numbers.Count();
+
+//Take, Skip (takewhile - bere dokud nenarazí na kladný výsledek podmínky, pak přestane)
+//var result = numbers.TakeWhile(n => n > 0);
+
+// Select - transformace
+//var result = numbers.Select(n => Math.Abs(n));
+
+
+
+
+
+
+
+
+
+
+foreach (int item in result)
 {
-    var result = FreqAnalysis.FreqAnalysisFromFile(file);
-    var fileInfo = new FileInfo(file); 
-   
-    Console.WriteLine(fileInfo.Name);
-    
-    var orderedTop10 = result.Words.OrderByDescending(kv => kv.Value).Take(10);
+    Console.WriteLine(item);
+}
 
-    foreach (var item in orderedTop10)
+
+static void FreqWords()
+{
+    var booksdir = @"C:\Users\Student\source\repos\starburst2006\Books";
+
+    var files = Directory.EnumerateFiles(booksdir, "*.txt");
+    foreach (var file in files)
     {
-        Console.WriteLine($"{item.Key}: {item.Value}");
-    }
+        var result = FreqAnalysis.FreqAnalysisFromFile(file);
+        var fileInfo = new FileInfo(file);
 
-    Console.WriteLine();
+        Console.WriteLine(fileInfo.Name);
+
+        var orderedTop10 = result.Words.OrderByDescending(kv => kv.Value).Take(10);
+
+        foreach (var item in orderedTop10)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+        Console.WriteLine();
+    }
 }
