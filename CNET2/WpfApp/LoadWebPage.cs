d42c73a476f3d99073be14af9367d3045332987e
Marek Bohanes <m.bohanes@gmail.com>
2022-06-14 16:14:49 +0200

    touple doplněn o boolean true nebo false

--- CNET2/WpfApp/LoadWebPage.cs
+++ CNET2/WpfApp/LoadWebPage.cs
@@ -10,21 +10,21 @@ namespace WpfApp
 {
     public static class LoadWebPage
     {
-        public static (int Length, string Url) LoadUrl(string url)
+        public static (int Length, string Url, bool success) LoadUrl(string url)
         {
 
             try
             {
                 HttpClient httpClient = new HttpClient();
                 var content = httpClient.GetStringAsync(url).Result;
-                return (content.Length, url);
+                return (content.Length, url, true);
 
             }
             catch (Exception ex)
             {
                 // todo log
                 File.AppendAllText("errors.txt", $"{DateTime.Now} {ex.Message}{Environment.NewLine}");
-                return (-1, url);
+                return (0, url, false);
             }        
         }
     }
